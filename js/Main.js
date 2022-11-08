1f3ea19c5857663a8c12c965f647bdf51dab954e
KIANIX21 <Nofal213456@gmail.com>
2022-11-08 21:12:23 +0700

    change main

--- js/Main.js
+++ js/Main.js
@@ -22,25 +22,6 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// Scroll section active link
-const sections = document.querySelectorAll('section[id]')
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop -50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
 // change background header 
 function scrollHeader(){
     const nav = document.getElementById('header')
